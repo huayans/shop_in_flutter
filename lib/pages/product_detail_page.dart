11b6d74b5dfb5a955174ec189b7563015c70dd53
HuayanS <huayansantiago@gmail.com>
2023-05-05 18:26:37 +0100

    product detail page adicionado

---
+++ lib/pages/product_detail_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+import '../models/product.dart';
+
+class ProductDetailPage extends StatelessWidget {
+  final Product product;
+
+  const ProductDetailPage({
+    required this.product,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
