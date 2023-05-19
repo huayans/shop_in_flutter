dcd6064309981fcb7c7bed06b5472e3de3f28ac4
HuayanS <huayansantiago@gmail.com>
2023-05-19 12:00:25 +0100

    refatoraçao do grid

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,34 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:shop/components/product_item.dart';
 
-import 'package:shop/models/product.dart';
-import 'package:shop/models/product_list.dart';
+import '../components/product_grid.dart';
 
 class ProdutcsOverViewPage extends StatelessWidget {
   const ProdutcsOverViewPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final provider = Provider.of<ProductList>(context);
-    final List<Product> loadedProducts = provider.items;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Minha Loja'),
       ),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(10),
-        itemCount: loadedProducts.length,
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-        itemBuilder: (ctx, i) => ProductItem(
-          product: loadedProducts[i],
-        ),
-      ),
+      body: const ProductGrid(),
     );
   }
 }
