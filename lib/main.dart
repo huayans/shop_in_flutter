7832963ed4e2a1e75a0f5e9cbb2ceb92ec08a03c
HuayanS <huayansantiago@gmail.com>
2023-05-19 10:41:16 +0100

    criando o product list

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/product_list.dart';
 import 'package:shop/pages/counter_detail_page.dart';
 import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/pages/products_overview_page.dart';
@@ -15,7 +17,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return CounterProvider(
+    return ChangeNotifierProvider(
+      create: (_) => ProductList(),
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
@@ -27,7 +30,7 @@ class MyApp extends StatelessWidget {
           fontFamily: 'Lato',
         ),
         home: ProdutcsOverViewPage(),
-        routes: {AppRoutes.product_detail: (ctx) => CounterDetailPage()},
+        routes: {AppRoutes.product_detail: (ctx) => ProductDetailPage()},
         debugShowCheckedModeBanner: false,
       ),
     );
--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/components/product_item.dart';
 
-import 'package:shop/data/dummy_data.dart';
-
 import 'package:shop/models/product.dart';
+import 'package:shop/models/product_list.dart';
 
 class ProdutcsOverViewPage extends StatelessWidget {
-  final List<Product> loadedProducts = dummyProducts;
-  ProdutcsOverViewPage({super.key});
+  const ProdutcsOverViewPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
+    final List<Product> loadedProducts = provider.items;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Minha Loja'),
