9149c96c462dde4ddf7fa5afdc786dadde03dfa0
HuayanS <huayansantiago@gmail.com>
2023-05-25 18:50:15 +0100

    botao de favorito funcional

--- lib/components/product_grid.dart
+++ lib/components/product_grid.dart
@@ -11,17 +11,21 @@ class ProductGrid extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final provider =Provider.of<ProductList>(context);
-    final List<Product> loadedProducts =provider.items;
-    return  GridView.builder(padding: const EdgeInsets.all(10),
-    itemCount: loadedProducts.length,
-    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-      itemBuilder: (ctx, i)=>ProductItem(product: loadedProducts[i]),
+    final provider = Provider.of<ProductList>(context);
+    final List<Product> loadedProducts = provider.items;
+    return GridView.builder(
+      padding: const EdgeInsets.all(10),
+      itemCount: loadedProducts.length,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+      itemBuilder: (ctx, i) => ChangeNotifierProvider(
+        create: (_) => loadedProducts[i],
+        child: ProductItem(),
+      ),
     );
   }
 }
--- lib/components/product_item.dart
+++ lib/components/product_item.dart
@@ -1,27 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-import 'package:shop/models/product.dart';
-import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/utils/app_routes.dart';
 
-class ProductItem extends StatelessWidget {
-  final Product product;
-  const ProductItem({
-    super.key,
-    required this.product,
-  });
+import '../models/product.dart';
 
+class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final product = Provider.of<Product>(context);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         footer: GridTileBar(
           backgroundColor: Colors.black87,
           leading: IconButton(
-            icon: const Icon(Icons.favorite),
+            icon: Icon(
+                product.isFavorite ? Icons.favorite : Icons.favorite_border),
             color: Theme.of(context).colorScheme.secondary,
-            onPressed: () {},
+            onPressed: () {
+              product.toggleFavorite();
+            },
           ),
           title: Text(
             product.title,
